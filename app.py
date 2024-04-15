8c17e45d6c8150d4f4340f5e1600d9c0efc97fe6
rawrrawrpurpledinosaur <191292L@students.hci.edu.sg>
2024-04-15 13:50:56 +0800

    images actually loading now

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ c = conn.cursor()
 def index():
     pets = c.execute("SELECT * from pets").fetchall()
     pet_images = c.execute("SELECT * from pet_images").fetchall()
+    print(pet_images)
     return render_template('index.html', pets=pets, pet_images=pet_images)
 
 @app.route('/login', methods=['GET', 'POST'])
